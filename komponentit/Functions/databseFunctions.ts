1fdbd8ce85cdd3a5799565796f716ca0c37466fc
tema <35255039+spadre@users.noreply.github.com>
2025-03-24 10:29:17 +0200

    moved asyncstorage functions from FoodIbnformationSaverPage to databaseFunctions

--- komponentit/Functions/databseFunctions.ts
+++ komponentit/Functions/databseFunctions.ts
@@ -1,4 +1,7 @@
-import AsyncStorage from "@react-native-async-storage/async-storage";
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import { Ingredient } from '../Objects/ingredientObject';
+
+
 
 export const storeData = async (dataKey:string, dataValue:string)=> {
     try {
@@ -17,16 +20,87 @@ export const getData = async (dataKey:string) => {
         }
     } catch (error) {
     }
-    
+
 };
 
-export const getAllKeys = async () => {
-    const keys:string[] = [];
-    try {
-        keys.push(await AsyncStorage.getAllKeys());
 
-    } catch (error) {
 
+export async function saveFoodInformation(name:string, category:string, manuF:string, kcals:string, proteins:string, fats:string, carbs:string){
+    name = name.toLowerCase();
+    category = category.toLowerCase();
+    manuF = manuF.toLowerCase();
+    const keys = await AsyncStorage.getAllKeys();
+    const allKeys = await AsyncStorage.multiGet(keys);
+
+    const ingredient = new Ingredient(name, category, manuF, Number(kcals), Number(proteins), Number(fats), Number(carbs));
+
+    for (let i = 0; i < allKeys.length; i++) {
+        console.log('allkeys[i][0]: ' + allKeys[i] + ' allkeys[i][1]: ' + allKeys[i][1]);
+        if(allKeys[i][0] === name && allKeys[i][1] === category){
+            console.log('löyty meinaan');
+        }
     }
-    console.log(keys[0]);
+
+    console.log(ingredient);
+
+    storeData('ingredient_' + ingredient.name,JSON.stringify(ingredient));
+    getData(ingredient.name);
 }
+
+export const saveFoodCategory = async (category:string) =>{
+    category = category.toLowerCase();
+    let testMatch:boolean = false;
+    const keys = await AsyncStorage.getAllKeys();
+    const allKeys = await AsyncStorage.multiGet(keys);
+
+
+
+    for (let i = 0; i < allKeys.length; i++) {
+        if(allKeys[i][0].includes('category_' + category)){
+            console.log('löyty ' + allKeys[i]);
+            testMatch = true;
+        }
+    }
+
+    if( testMatch === false){
+        AsyncStorage.setItem('category_' + category,category);
+        console.log('Stored key');
+    }else {console.log('Key already exists');}
+
+    console.log(AsyncStorage.getItem(category + '_1'));
+
+};
+
+export const deleteFoodCategory = async(category:string) => {
+    category = category.toLowerCase();
+    category = category + '_category';
+    const keys = await AsyncStorage.getAllKeys();
+    const allKeys = await AsyncStorage.multiGet(keys);
+
+
+    for(let i = 0; i < allKeys.length; i++){
+        if(allKeys[i][0].includes(category)){
+            console.log(allKeys[i][0]);
+            await AsyncStorage.removeItem(category);
+        }
+    }
+    //await AsyncStorage.multiRemove(keys)
+    console.log(allKeys);
+};
+
+//"debug"
+export const showAllKeys = async() => {
+    const keys = await AsyncStorage.getAllKeys();
+    const allKeys = await AsyncStorage.multiGet(keys);
+    if(allKeys.length > 0){
+        for (let i =  0; i < allKeys.length; i++){
+            console.log(allKeys[i]);
+
+        }} else {console.log("No keys found");}
+};
+export const deleteAllKeys = async() => {
+    const keys = await AsyncStorage.getAllKeys();
+    await AsyncStorage.multiRemove(keys);
+        console.log(keys);
+};
+
--- komponentit/Pages/FoodInformationSaverPage.tsx
+++ komponentit/Pages/FoodInformationSaverPage.tsx
@@ -1,10 +1,9 @@
 import { Button, StyleSheet, View } from 'react-native';
 import InputComp from '../Components/InputComp';
 import { useState } from 'react';
-import { storeData, getData } from '../Functions/databseFunctions';
-import { Ingredient } from '../Objects/ingredientObject';
+import {deleteFoodCategory, saveFoodCategory, saveFoodInformation,showAllKeys, deleteAllKeys } from '../Functions/databseFunctions';
 import { processNumbers } from '../Functions/processNumber';
-import AsyncStorage from '@react-native-async-storage/async-storage';
+
 
 const FoodInformationSaverPage = () => {
     const [name, setName] = useState(String);
@@ -22,6 +21,8 @@ const FoodInformationSaverPage = () => {
                     <Button title="Save ingredient" onPress={() => {saveFoodInformation(name, category,  manuf, kcals, proteins, Fats, carbs)}}/>
                     <Button title="Save new category" onPress={() => {saveFoodCategory(category)}}/>
                     <Button title="Delete category" onPress={() => {deleteFoodCategory(category)}}/>
+                    <Button title="Show All category(debug)" onPress={() => {showAllKeys()}}/>
+                    <Button title="Delete All category(debug)" onPress={() => {deleteAllKeys()}}/>
                     <InputComp label="Name" labelColor={styles.labelColor} inputKeybordType="text" inputFunctio={newText => setName(newText)} inputValue={name}/>
                     <InputComp label="Category" labelColor={styles.labelColor} inputKeybordType="search" inputFunctio={newText => setCategory(newText)} inputValue={category}/>
                     <InputComp label="Manufacturer" labelColor={styles.labelColor} inputKeybordType="text" inputFunctio={newText => setManuf(newText)} inputValue={manuf}/>
@@ -34,56 +35,9 @@ const FoodInformationSaverPage = () => {
 
 };
 
-function saveFoodInformation(name:string, category:string, manuF:string, kcals:string, proteins:string, fats:string, carbs:string){
-
-    const ingredient = new Ingredient(name, category, manuF, Number(kcals), Number(proteins), Number(fats), Number(carbs));
-
-    console.log(ingredient);
-
-    storeData(ingredient.name+'_1',JSON.stringify(ingredient));
-    getData(ingredient.name+'_1');
-}
-
-const saveFoodCategory = async (category:string) =>{
-    category = category.toLowerCase();
-    let testMatch:boolean = false;
-    const keys = await AsyncStorage.getAllKeys();
-    const allKeys = await AsyncStorage.multiGet(keys);
 
 
 
-    for (let i = 0; i < allKeys.length; i++) {
-        if(allKeys[i][0].includes(category)){
-            console.log('löyty ' + allKeys[i]);
-            testMatch = true;
-        }
-    }
-
-    if( testMatch === false){
-        AsyncStorage.setItem(category + '_category',category);
-        console.log('Stored key');
-    }else {console.log('Key already exists');}
-
-    console.log(AsyncStorage.getItem(category + '_1'));
-
-};
-
-const deleteFoodCategory = async(category:string) => {
-    category = category.toLowerCase();
-    category = category + '_category';
-    const keys = await AsyncStorage.getAllKeys();
-    const allKeys = await AsyncStorage.multiGet(keys);
-
-
-    for(let i = 0; i < allKeys.length; i++){
-        if(allKeys[i][0].includes(category)){
-            console.log(allKeys[i][0]);
-            await AsyncStorage.removeItem(category);
-        }
-    }
-    //await AsyncStorage.multiRemove(keys)
-    console.log(allKeys);
-}
 
 
 const styles = StyleSheet.create({
